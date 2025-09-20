be97a468732dfa446f3bec1bba69548b47e7c58e
vpriyadharshini29 <vpriyadharshini29@gmail.com>
2025-09-20 12:54:04 +0530

    Fullstack project-Shoe website

--- src/api/shoeapi.jsx
+++ src/api/shoeapi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const shoeApi = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/shoe/`, // uses env var
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/shoe/`, // uses env var
 });
 
 export default shoeApi;
--- src/api/userapi.jsx
+++ src/api/userapi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const usersApi = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/auth/`, // uses env var
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/auth/`, // uses env var
 });
 
 export default usersApi;
